3f9a4024e7f3af7e2a3ff8204dbe76c9e880182e
h587904 <70509944+h587904@users.noreply.github.com>
2020-11-12 13:05:57 +0100

    jp11

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -4,12 +4,34 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 import no.hvl.dat100.jplab11.common.TODO;
+import no.hvl.dat100.jplab11.oppgave2.Bilde;
+import no.hvl.dat100.jplab11.oppgave2.Tekst;
 import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg {
-
+	
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		String test = samling.toString();
+
+		PrintWriter skriver = null;
+		try {
+			java.io.File file = new java.io.File(mappe + filnavn);
+			skriver = new PrintWriter(file);
+			skriver.println(test);
+			
+		}
+		catch (FileNotFoundException e) {
+			System.out.println(e);
+			return false;
+			
+		}
+		finally {
+			if(skriver != null) 
+				skriver.close();
+		}
+		
+		return true;
+		
 	}
 }
